5fa20616696af2976f5000ce8f27e975fd1af3be
Nicolas Larralde <n-larralde@users.noreply.github.com>
2023-02-21 18:06:23 -0500

    Update Globals.h

--- Source/TP2/Globals.h
+++ Source/TP2/Globals.h
@@ -9,7 +9,7 @@ namespace SF
 
     using ulong = unsigned long;
     using PresetList = std::array<ulong, 9>;
-    inline static const PresetList _presets = { 470000, 480000, 490000, 500000, 510000, 520000, 530000, 540000, 550000 };
+    inline static const PresetList _presets = { 5000, 50000, 75000, 100000, 150000, 200000, 300000, 400000, 500000 };
 
     static const char* WINDOW_TITLE = "Snow simulator";
     const int SCREEN_WIDTH  = 720;
